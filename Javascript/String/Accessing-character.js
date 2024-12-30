6fd7158de9149a631dc1f7efa47f56efd9426e32
Vikash kumar <vikash08.kumar1995@gmail.com>
2024-12-30 12:38:54 +0530

    string practice

---
+++ Javascript/String/Accessing-character.js
@@ -0,0 +1,9 @@
+//use charA() to get a character at the specified index
+
+var string='Hello world';
+console.log(string.charAt(0)); //H
+
+//bcz strings can be treated like array of characters hence can be accessed using index
+console.log(string[0]); //H
+//use charCodeAt() to get the unicode of the character at the specified index
+console.log(string.charCodeAt(0)); //72
