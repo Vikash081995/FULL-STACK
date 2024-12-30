da1c859b2ff52e5c209ade0a366bcddc26aa16f4
Vikash kumar <vikash08.kumar1995@gmail.com>
2024-12-30 12:20:43 +0530

    compairing string

---
+++ Javascript/String/reverse-string.js
@@ -0,0 +1,15 @@
+
+function reverseString(str){
+    return str.split('').reverse().join('');
+}
+
+reverseString("hello"); //"olleh"
+
+//using spread operator 
+function revString(str){
+    return [...String(str)].reverse().join('');
+}
+
+console.log(revString("hello")); //"olleh"
+console.log(revString(1234)); //"4321"
+console.log(revString([1,2,3])); //"eurt"
