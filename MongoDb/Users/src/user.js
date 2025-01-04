ec05b5253437092108a39472d395545cae98d693
Vikash kumar <vikash08.kumar1995@gmail.com>
2025-01-04 13:11:55 +0530

    post schema added

--- MongoDb/Users/src/user.js
+++ MongoDb/Users/src/user.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const PostSchema = require("./post");
+
 
 const UserSchema = new Schema({
   name: {
@@ -11,6 +13,7 @@ const UserSchema = new Schema({
     required: [true, "Name is required"],
   },
   postCount: Number,
+  posts: [PostSchema],
 });
 
 const User = mongoose.model("user", UserSchema);
