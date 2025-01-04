35cbc1fe76734d63d1a9d6401003bc0255606f8c
Vikash kumar <vikash08.kumar1995@gmail.com>
2025-01-04 13:07:09 +0530

    testing validation

--- MongoDb/Users/test/validation_test.js
+++ MongoDb/Users/test/validation_test.js
@@ -10,19 +10,18 @@ describe("Validation records", () => {
       assert(message === "Name is required");
     });
   });
-  it('requires a users name longer than 2 characters',()=>{
-    const user = new User({name:'Al'})
-    const validationResult = user.validateSync()
-    const {message} = validationResult.errors.name
-    assert(message==='Name must be longer than 2 characters')
-  })
-  it('disallows invalid records from being saved',(done)=>{
-    const user = new User({name:'Al'})
-    user.save()
-    .catch((validationResult)=>{
-      const {message} = validationResult.errors.name
-      assert(message==='Name must be longer than 2 characters')
-      done()
-    })
-  })
+  it("requires a users name longer than 2 characters", () => {
+    const user = new User({ name: "Al" });
+    const validationResult = user.validateSync();
+    const { message } = validationResult.errors.name;
+    assert(message === "Name must be longer than 2 characters");
+  });
+  it("disallows invalid records from being saved", (done) => {
+    const user = new User({ name: "Al" });
+    user.save().catch((validationResult) => {
+      const { message } = validationResult.errors.name;
+      assert(message === "Name must be longer than 2 characters");
+      done();
+    });
+  });
 });
