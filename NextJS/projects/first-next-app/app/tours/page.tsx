2b71b43a34a38aa651aa200e090232cdc9ec875d
Vikash kumar <vikash08.kumar1995@gmail.com>
2025-01-09 02:10:31 +0530

    nex-js data fetching

---
+++ NextJS/projects/first-next-app/app/tours/page.tsx
@@ -0,0 +1,29 @@
+const url = "http://localhost:3000/api/tours";
+type Tour = {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+};
+
+const fetchTours = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const response = await fetch(url);
+  const data: Tour[] = await response.json();
+  return data;
+};
+
+async function ToursPage() {
+  const data = await fetchTours();
+
+  return (
+    <section>
+      <h1 className="text-3xl mb-4">Tours</h1>
+      {data.map((tour: Tour) => {
+        return <h2 key={tour.id}>{tour.name}</h2>;
+      })}
+    </section>
+  );
+}
+
+export default ToursPage;
