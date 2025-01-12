b862c4f0fd262ca1a0c6ea94d3fbc5992f3ccd04
Vikash kumar <vikash08.kumar1995@gmail.com>
2025-01-12 13:27:53 +0530

    learning packages

--- Java/Hello.java
+++ Java/Hello.java
@@ -1,5 +1,6 @@
 import CoreJava.Calc;
 import CoreJava.AdvancedCal;
+import java.util.*;
 
 public class Hello {
     public static void main(String[] args) {
@@ -11,5 +12,13 @@ public class Hello {
         System.out.println(adv.mul(2, 3));
         System.out.println(adv.div(2, 3));
         System.out.println(adv.add(2, 3));
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        System.out.println(list);
     }
 }
