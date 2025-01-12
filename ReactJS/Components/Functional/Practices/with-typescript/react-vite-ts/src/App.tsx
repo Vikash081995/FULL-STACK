62470d08828d5df0f7c0a3f4dceadab0e3875f2d
Vikash kumar <vikash08.kumar1995@gmail.com>
2025-01-13 02:21:51 +0530

    react with typescript

---
+++ ReactJS/Components/Functional/Practices/with-typescript/react-vite-ts/src/App.tsx
@@ -0,0 +1,15 @@
+import Heading from "./components/Heading";
+import Section from "./components/Section";
+import Counter from "./components/Counter";
+
+function App() {
+  return (
+    <>
+      <Heading title={"hello"} />
+      <Section title={"Difference"}>This is my Section</Section>
+      <Counter />
+    </>
+  );
+}
+
+export default App;
