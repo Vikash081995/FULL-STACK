55e596899e330a2a1dc6dc59571258199bf8a2c8
Vikash kumar <vikash08.kumar1995@gmail.com>
2025-01-31 19:18:46 +0530

    react practice

---
+++ ReactJS/React-complete-e2e/src/components/Table.jsx
@@ -0,0 +1,15 @@
+import Columns from "./Columns"
+
+function Table() {
+  return (
+  <table>
+        <tbody>
+            <tr>
+                <Columns />
+            </tr>
+        </tbody>
+  </table>
+  )
+}
+
+export default Table
