6f827c5b558fb3ae1056cb05862e3b755595e514
Vikash kumar <vikash08.kumar1995@gmail.com>
2025-02-02 16:48:55 +0530

    react practice

---
+++ ReactJS/Functional/Theory/PassingFunctionAsProps/Button.jsx
@@ -0,0 +1,7 @@
+function Button({ onClick }) {
+  return (
+    <button type="button" onClick={onClick}>
+      click me{" "}
+    </button>
+  );
+}
---
+++ ReactJS/Functional/Theory/PassingFunctionAsProps/Example.jsx
@@ -0,0 +1,7 @@
+function Example() {
+  const handleClick = () => alert("clicked");
+
+  return <Button onClick={handleClick}>Click Me</Button>;
+}
+
+export default Example;
