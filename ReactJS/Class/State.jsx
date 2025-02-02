92305535c0019a443a14928c62fce7fb8ffcfb87
Vikash kumar <vikash08.kumar1995@gmail.com>
2025-02-02 20:39:26 +0530

    react practice

---
+++ ReactJS/Class/State.jsx
@@ -0,0 +1,27 @@
+import React,{Component} from 'react';
+
+class Car extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            brand:"ford",
+            model:"mustang",
+            year:"1964",
+            color:"red"
+        }
+    }
+
+    changeColor=()=>{
+        this.setState({color:'blue'})
+    }
+    render(){
+        return(
+            <div>
+                <h1>My {this.state.brand}</h1>
+                <p>
+                    It is a {this.state.color} {this.state.model} from {this.state.year}
+                </p>
+            </div>
+        )
+    }
+}
