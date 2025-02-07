2233c8fe5f1ce86cc10ea88d3157777b60e8adba
Vikash kumar <vikash08.kumar1995@gmail.com>
2025-02-08 02:17:32 +0530

    Arrow functions

---
+++ Javascript/Practice/Arrow-functions.js
@@ -0,0 +1,50 @@
+const sum = (a,b)=>a+b;
+console.log(sum(1,2));
+
+// ====================================================================
+//IMPLICIT RETURN 
+
+const getLength = str=>str.length;
+
+console.log(getLength('Hello'));    //'output: 5'
+
+// ====================================================================
+// NO PARAMETERS
+
+const greet=()=>'HELLO WORLD';
+console.log(greet());
+// ================================================================
+//Single parameters 
+
+const numbers =[1,2,3,44,5,6,7,8,9,10];
+const double = numbers.filter((num)=>{
+    return num*2;
+})
+console.log(double);
+
+//==================================================================
+// Array methods 
+
+const numbers1 = [1,2,3,4,5,6,7,8,9,10];
+
+const squares =numbers1.map(num=>num*num);
+console.log(squares);
+//==================================================================
+// Object methods
+
+const person ={
+    name:"Alice",
+    getName:()=>person.name
+}
+console.log(person.getName()); // Output: "Alice"
+//==================================================================
+//Default parameters
+
+const multiply = (a,b=1)=>a*b;
+console.log(multiply(5,2)); // Output: 10
+console.log(multiply(5)); // Output: 5
+/// =================================================================
+//REST PARAMETERS
+const sumAll =(...numbers)=>numbers.reduce((acc,num)=>acc+num,0)
+console.log(sumAll(1,2,3,4,5)); // Output: 15
+//==================================================================
