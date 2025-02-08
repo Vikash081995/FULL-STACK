e40f763a21d4e132a5e59034342b607cde7247c4
Vikash kumar <vikash08.kumar1995@gmail.com>
2025-02-08 17:40:45 +0530

    closures

--- Javascript/Practice/Closure.js
+++ Javascript/Practice/Closure.js
@@ -10,7 +10,7 @@ function outerFunction() {
 const Inner = outerFunction();
 Inner(); //I am from outer function
 
-//===   ==================================================
+//=====================================================
 // Private variables
 function createPrivateVariables() {
   let privateVariable = 0;
@@ -28,7 +28,7 @@ const privateVar = createPrivateVariables();
 console.log(privateVar.get()); // 0
 privateVar.set(10);
 console.log(privateVar.get()); // 10
-//===   ==================================================
+//=====================================================
 
 function createCounter() {
   let count = 0;
@@ -83,16 +83,65 @@ counter1(); //1
 counter1(); //2
 counter1(); //3
 //=======================================================
-//Event Handler 
-function addClickHandler(button){
-    let clickCount=0;
-    button.addEventListener('click',()=>{
-        clickCount++;
-        console.log(clickCount);
-    })
+//Event Handler
+function addClickHandler(button) {
+  let clickCount = 0;
+  button.addEventListener("click", () => {
+    clickCount++;
+    console.log(clickCount);
+  });
 }
 
-const button =document.createElement('button');
+const button = document.createElement("button");
 document.body.appendChild(button);
 addClickListener(button);
 //=======================================================
+//closure in loops fixing var issues
+
+for (var i = 0; i <= 3; i++) {
+  setTimeout(() => {
+    console.log(i); // All logs "4" because `var` is function-scoped
+  }, 1000);
+}
+
+// Fix using a closure
+for (var i = 0; i <= 3; i++) {
+  (function (index) {
+    setTimeout(() => {
+      console.log(index); // Logs 0, 1, 2, 3
+    }, 1000);
+  })(i);
+}
+
+//=======================================================
+//Memoization
+function memoized() {
+  let cache = {};
+  return function (n) {
+    if (n in cache) {
+      console.log("Fetching from cache", n);
+      return cache[n];
+    } else {
+      console.log("Calculating result", n);
+      let result = n + 10;
+      cache[n] = result;
+      return result;
+    }
+  };
+}
+
+const addTen = memoized();
+console.log(addTen(5)); //// Computing result for: 5 → Output: 15
+console.log(addTen(5)); // Fetching from cache for: 5 → Output: 15
+//=======================================================
+
+function makeArmy() {
+  let shooters = [];
+  for (let i = 0; i < 10; i++) {
+    const shooter = () => {
+      console.log(i);
+    };
+    shooters.push(shooter);
+  }
+  return shooters;
+}
