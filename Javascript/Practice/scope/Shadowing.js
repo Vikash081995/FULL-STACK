eb88995d11659b259289356001aaadd4aee2a884
Vikash kumar <vikash08.kumar1995@gmail.com>
2025-02-13 19:13:33 +0530

    refactored folders

--- Javascript/Practice/scope/Shadowing.js
+++ Javascript/Practice/scope/Shadowing.js
@@ -2,7 +2,7 @@ var a = "global";
 
 function foo() {
   var a = "local";
-  console.log("inside a" + a); //insidelocal
+  console.log("inside a" + a); //inside a local
 }
 foo();
 console.log("outside a=" + a); //outside a=global
