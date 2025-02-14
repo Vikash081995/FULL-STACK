f96c1a52195479649b402c61f922010dbac1347e
Vikash kumar <vikash08.kumar1995@gmail.com>
2025-02-14 23:11:02 +0530

    javascript practice

--- Javascript/Practice/type-converison/String-Conversion.js
+++ Javascript/Practice/type-converison/String-Conversion.js
@@ -5,3 +5,23 @@ let str = num.toString();
 let result = num + str;
 console.log(result); //4242
 console.log(typeof result); //string
+
+//================================
+let bool=true;
+let st1r = bool.toString();
+console.log(st1r); //"true"
+
+//================================
+//using template literals
+let num1=43;
+let str3=`${num1}`
+console.log(str3); //43
+
+//================================
+console.log("5" + 5); //55
+console.log(5 + "5"); //55
+console.log("5" - 5); //0
+console.log(5 - "5"); //0
+console.log(true + "is concatnated");
+console.log(null + "is null");
+console.log(undefined + "is undefined ");
