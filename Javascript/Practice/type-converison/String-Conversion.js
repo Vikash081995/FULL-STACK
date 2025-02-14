06bf403f7b9a6d679886cb6389b45313b3ffcd33
Vikash kumar <vikash08.kumar1995@gmail.com>
2025-02-14 13:58:38 +0530

    string conversion

---
+++ Javascript/Practice/type-converison/String-Conversion.js
@@ -0,0 +1,7 @@
+//String conversion
+//================================
+let num = 42;
+let str = num.toString();
+let result = num + str;
+console.log(result); //4242
+console.log(typeof result); //string
