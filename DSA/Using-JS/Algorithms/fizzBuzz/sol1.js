8ecd658f27474f38d8062fe308acd114bc1ddef1
Vikash kumar <vikash08.kumar1995@gmail.com>
2025-02-16 23:14:58 +0530

    dsa

--- DSA/Using-JS/Algorithms/fizzBuzz/sol1.js
+++ DSA/Using-JS/Algorithms/fizzBuzz/sol1.js
@@ -4,20 +4,20 @@
 //
 //n = 15,
 //
-//Return:   
-//    
+//Return:
+//
 //   1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz.
 
-function fizzBuzz(n){
-    for(let i=1;i<=n;i++){
-        if(i%3===0 && i%5===0){
-            console.log("FizzBuzz");
-        }else if(i%3===0){
-            console.log("Fizz");
-        }else if(i%5===0){
-            console.log("Buzz");
-        }else{
-            console.log(i);
-        }
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
     }
+  }
 }
