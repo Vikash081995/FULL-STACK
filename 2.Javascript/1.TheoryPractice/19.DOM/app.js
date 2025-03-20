62ce8560c69fc67826e8735bfe1f1365f000add5
Vikash kumar <vikash08.kumar1995@gmail.com>
2025-03-21 01:01:31 +0530

    dom practice

--- 2.Javascript/1.TheoryPractice/19.DOM/app.js
+++ 2.Javascript/1.TheoryPractice/19.DOM/app.js
@@ -1,3 +1,7 @@
+//DOM(Document Object Model)
+//It is a programming interface for web documents.
+//It represents the structure of the document as a tree of objects.
+
 const img = document.getElementById("image");
 console.dir(img);
 
@@ -13,24 +17,37 @@ console.log(header, ul);
 const h1 = document.querySelector("h1");
 console.log(h1);
 
-
-const pg =document.querySelector('p');
+const pg = document.querySelector("p");
 console.log(pg);
 
-const selectImage = document.querySelector('img');
+const selectImage = document.querySelector("img");
 console.log(selectImage);
 
-const special = document.querySelector('.special');
+const special = document.querySelector(".special");
 console.log(special);
 
-const lispeacial = document.querySelector('li.special');
+const lispeacial = document.querySelector("li.special");
 console.log(lispeacial);
 
-const inputs = document.querySelectorAll('input');
+const inputs = document.querySelectorAll("input");
 console.log(inputs);
 
-const specialClass = document.querySelectorAll('.special');
+const specialClass = document.querySelectorAll(".special");
 console.log(specialClass);
 
-const lis = document.querySelectorAll('li');
+const lis = document.querySelectorAll("li");
 console.log(lis);
+
+///----------------------------------------
+//Value,src,href
+//----------------------------------------
+const AllInputs = document.querySelectorAll("input");
+console.log(AllInputs, "all inputs");
+console.log(AllInputs[0].value);
+inputs[0].value = "hello";
+console.log(AllInputs[0].value);
+console.log(AllInputs[1].placeholder);
+const anchor = document.querySelector("a");
+console.log(anchor.href);
+const image = document.querySelector("img");
+console.log(image.src);
