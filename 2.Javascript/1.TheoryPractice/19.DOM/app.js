f47e989e74ee474a236b2b16bb6d830294bb902f
Vikash kumar <vikash08.kumar1995@gmail.com>
2025-03-21 00:43:51 +0530

    nidejs and js

---
+++ 2.Javascript/1.TheoryPractice/19.DOM/app.js
@@ -0,0 +1,36 @@
+const img = document.getElementById("image");
+console.dir(img);
+
+const getElementByTag = document.getElementsByTagName("p");
+console.dir(getElementByTag);
+
+const header = document.getElementsByClassName("header");
+
+const ul = document.getElementsByTagName("ul")[0];
+
+console.log(header, ul);
+
+const h1 = document.querySelector("h1");
+console.log(h1);
+
+
+const pg =document.querySelector('p');
+console.log(pg);
+
+const selectImage = document.querySelector('img');
+console.log(selectImage);
+
+const special = document.querySelector('.special');
+console.log(special);
+
+const lispeacial = document.querySelector('li.special');
+console.log(lispeacial);
+
+const inputs = document.querySelectorAll('input');
+console.log(inputs);
+
+const specialClass = document.querySelectorAll('.special');
+console.log(specialClass);
+
+const lis = document.querySelectorAll('li');
+console.log(lis);
