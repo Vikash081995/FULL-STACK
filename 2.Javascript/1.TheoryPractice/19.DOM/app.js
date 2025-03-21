4a8a40b8a731b465ada7c1398397ec84d5bd16ba
Vikash kumar <vikash08.kumar1995@gmail.com>
2025-03-21 10:29:19 +0530

    DOM Practice

--- 2.Javascript/1.TheoryPractice/19.DOM/app.js
+++ 2.Javascript/1.TheoryPractice/19.DOM/app.js
@@ -51,3 +51,64 @@ const anchor = document.querySelector("a");
 console.log(anchor.href);
 const image = document.querySelector("img");
 console.log(image.src);
+//----------------------------------------
+//getting and setting attributes
+//----------------------------------------
+const range = document.querySelector('input[type="range"]');
+console.log(range.getAttribute('max'))
+console.log(range.getAttribute('min'))
+range.setAttribute('min', '-500')
+console.log(range.getAttribute('min'))
+//===========================================
+//Finding parent, children, siblings
+//===========================================
+const firstLI = document.querySelector("li");
+console.log(firstLI.parentElement);
+console.log(firstLI.parentElement.parentElement);
+console.log(firstLI.parentElement.parentElement.parentElement);
+const firstul = document.querySelector("ul");
+console.log(firstul.children);
+console.log(firstul.children[0]);
+const firstLi=  document.querySelector("li");
+console.log(firstLi.previousElementSibling);
+console.log(firstLi.nextElementSibling);
+console.log(firstLi.nextElementSibling.nextElementSibling);
+//===========================================
+//changing multiple elements
+//===========================================
+const allLis = document.querySelectorAll("li");
+
+for(let i=0;i<allLis.length;i++){
+    console.log(allLis[i].innerText)
+}
+
+for(let li of allLis){
+    li.innerHTML = "WE ARE <b>THE CHAMPIONS</b>"
+}
+//===========================================
+//changing styles
+//===========================================
+const p = document.querySelector("p");
+console.log(p.style.color);
+p.style.color = "red";
+p.style.fontSize = "20px";
+p.style.backgroundColor = "yellow";
+//===========================================
+const allLIS = document.querySelectorAll("li");
+const colors= ["red","orange","yellow","green","blue","purple"];
+
+allLIS.forEach((li,i)=>{
+    const color = colors[i];
+    li.style.color = color;
+}
+)
+//===========================================
+//getComputedStyle
+//===========================================
+const h = document.querySelector("h1");
+const compStyles = getComputedStyle(h);
+console.log(compStyles.color);
+console.log(compStyles.fontSize);
+//===========================================
+//MANIPULATING CLASSES
+//===========================================
