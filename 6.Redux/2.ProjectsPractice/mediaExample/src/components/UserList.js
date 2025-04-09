a7c32aabec8ee4b453719e25e454f3b4ff5f4bd3
Vikash kumar <vikash08.kumar1995@gmail.com>
2025-04-09 17:48:05 +0530

    redux toolkit

--- 6.Redux/2.ProjectsPractice/mediaExample/src/components/UserList.js
+++ 6.Redux/2.ProjectsPractice/mediaExample/src/components/UserList.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchUsers } from "../stores";
+import { fetchUsers, addUser } from "../stores";
 import { usersReducer } from "./Skeleton";
+import Button from "./Button";
 import Skeleton from "./Skeleton";
 
 function UserList() {
@@ -9,6 +10,10 @@ function UserList() {
   const { data, isLoading, error } = useSelector((state) => {
     return state.users;
   });
+
+  const handleUserAdd = () => {
+    dispatch(addUser());
+  };
 
   useEffect(() => {
     dispatch(fetchUsers());
@@ -25,7 +30,15 @@ function UserList() {
   const renderUsers = data.map((user) => {
     return <div key={user.id}>{user.name}</div>;
   });
-  return <div>{renderUsers}</div>;
+  return (
+    <div>
+      <div>
+        <h1>Users</h1>
+        <Button onClick={handleUserAdd}>Add Users </Button>
+      </div>
+      {renderUsers}
+    </div>
+  );
 }
 
 export default UserList;
