0a4bad84a1c6192d9b75d010e9256526df120efe
Vikash kumar <vikash08.kumar1995@gmail.com>
2025-04-15 14:53:48 +0530

    javascript practice

---
+++ 12.DSA/Problem-solving-patterns/Multiple-Pointers.js
@@ -0,0 +1,8 @@
+//Example Question 
+//Write a function called SumZero which accepts a sorted array of integers as input and returns the first pair of integers in the array that add up to zero.
+//return an array that includes both values that sum to zero or undefined if pair doesn't exist.
+
+//SumZero([-3,-2,-1,0,1,2,3]) //[-3,3]
+//SumZero([-2,0,1,3]) //undefined
+//SumZero([1,2,3,4,5]) //undefined
+//=================================
