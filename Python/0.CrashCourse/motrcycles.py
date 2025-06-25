eb8e649b3b66815d9d47116c7897f6d03aeba8ae
Vikash kumar <vikash08.kumar1995@gmail.com>
2025-06-25 16:32:24 +0530

    python practice

--- Python/0.CrashCourse/motrcycles.py
+++ Python/0.CrashCourse/motrcycles.py
@@ -10,3 +10,16 @@ print(motorcycles)
 motorcycles.insert(0, 'honda')
 print(motorcycles)
 # ['honda', 'ducati', 'yamaha', 'suzuki', 'ducati', 'ducati']
+
+motors=[]
+motors.append('honda')
+motors.append('yamaha')
+motors.append('suzuki')
+motors.append('ducati')
+print(motors)
+# ['honda', 'yamaha', 'suzuki', 'ducati']
+last_used =motors.pop()
+print(motors)
+# ['honda', 'yamaha', 'suzuki']
+print(f"The last motorcycle I owned was a {last_used.title()}.")
+# The last motorcycle I owned was a Ducati.
