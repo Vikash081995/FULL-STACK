abb3b768e0c26ae2e3d58ad4dd756e1e21966d4a
Vikash kumar <vikash08.kumar1995@gmail.com>
2025-07-19 18:24:13 +0530

    project

--- Python/Tutorial/3.Conditions/if-else.py
+++ Python/Tutorial/3.Conditions/if-else.py
@@ -4,3 +4,13 @@ if meaning > 10:
     print("Right on!")
 else:
     print("Not today")
+
+
+#=================
+cars =["Toyota", "Honda", "Ford","bmw", "Mercedes"]
+
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
