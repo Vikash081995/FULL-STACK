9467571ee2b0d5b60fedc4b0605a2a77be80170e
Vikash kumar <vikash08.kumar1995@gmail.com>
2025-08-03 12:10:27 +0530

    ML-AI

---
+++ Python/Books-Learning/Python-Crash-Course/greeter.py
@@ -0,0 +1,5 @@
+prompt ="if you share your name, we can personalize the greeting. Would you like to share your name? (yes/no): "
+prompt += " (Type 'exit' to quit) "
+
+name = input(prompt)
+print(f"Hello, {name}! Welcome to the Python Crash Course.")
