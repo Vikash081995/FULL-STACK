c68e2776a78f01ccdd9b9b53912f68571dcf1256
Vikash kumar <vikash08.kumar1995@gmail.com>
2025-10-11 01:27:02 +0530

    express

--- NODEJS/01.LEARNING/Express/index.js
+++ NODEJS/01.LEARNING/Express/index.js
@@ -1,48 +1,13 @@
 const express = require("express");
-
+const { loggerMiddleware } = require("./middlewares/logger");
+const bookRouter = require("./routes/book.routes");
 const app = express();
-
-const books = [
-  { id: 1, name: "Book One" },
-  { id: 2, name: "Book Two" },
-  { id: 3, name: "Book Three" },
-  { id: 4, name: "Book Four" },
-  { id: 5, name: "Book Five" },
-];
 
 //middleware
 app.use(express.json());
-
-//Routes
-app.get("/books", (req, res) => {
-  res.json(books);
-});
-
-app.get("/books/:id", (req, res) => {
-  const bookId = parseInt(req.params.id, 10);
-  const book = books.find((b) => b.id === bookId);
-
-  if (!book) {
-    return res.status(404).send("Book not found");
-  }
-  res.json(book);
-});
-
-app.post("/books", (req, res) => {
-  const { title } = req.body;
-
-  if (!title || !author) {
-    return res.status(400).send("Title and Author are required");
-  }
-
-  const book = {
-    id: books.length + 1,
-    title,
-  };
-
-  books.push(book);
-  res.status(201).json(book);
-});
+app.use(loggerMiddleware);
+//routes
+app.use('/books', bookRouter);
 
 const PORT = process.env.PORT || 3000;
 
