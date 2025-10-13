f70c2eb18d839cdce30a2ee1cf7a7f04e7371a27
Vikash kumar <vikash08.kumar1995@gmail.com>
2025-10-14 01:09:47 +0530

    typescript practice

--- Typescript/Theory/1.basics/basics.ts
+++ Typescript/Theory/1.basics/basics.ts
@@ -1,16 +1,15 @@
-let pi:number = 3.14;
-let movieTitle:string = "The Godfather";
+let pi: number = 3.14;
+let movieTitle: string = "The Godfather";
 // movieTitle = 9; // Error: Type '9' is not assignable to type 'string'.
 movieTitle = "The Godfather 2";
 movieTitle.toUpperCase();
 
-let numCatLives:number=9;
-numCatLives+=1;
+let numCatLives: number = 9;
+numCatLives += 1;
 
-let numOver:boolean=true;
+let numOver: boolean = true;
 
-
-//Type Inference 
+//Type Inference
 let tvShow = "Olive kittens";
 tvShow = "Kittens";
 // tvShow=true; // Error: Type 'true' is not assignable to type 'string'.
@@ -18,26 +17,32 @@ tvShow = "Kittens";
 let isFunny = false;
 isFunny = true;
 
-
 //The any type
-let thing:any="hello";
-thing=1;
-thing=true;
+let thing: any = "hello";
+thing = 1;
+thing = true;
 thing();
 thing.toUpperCase();
 
 const movies = ["The Godfather", "The Dark Knight", "The Godfather 2"];
-let foundMovie:any;
+let foundMovie: any;
 
-for(let movie of movies){
-    if(movie === "The Dark Knight"){
-        foundMovie = movie;
-    }else{
-        foundMovie = "No movie found";
-    }
+for (let movie of movies) {
+  if (movie === "The Dark Knight") {
+    foundMovie = movie;
+  } else {
+    foundMovie = "No movie found";
+  }
 }
 
 foundMovie.toUpperCase();
 
+//----------------------------------------------------
+let rapper: string = "Kanye West";
+rapper.length; //ok
+// rapper.push("hello"); //error
 
-
+//----------------------------------------------------
+let evolvingAny;
+evolvingAny = 0;
+// evolvingAny(); //ok in ts but run time error
